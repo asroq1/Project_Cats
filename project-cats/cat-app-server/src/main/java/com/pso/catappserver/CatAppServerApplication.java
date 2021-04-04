b7e2342ce8804bb60f2c3de2686ccf6f961c8842
hayeon17kim <hayeon17kim@gmail.com>
2021-04-04 21:18:19 +0900

    init project

---
+++ project-cats/cat-app-server/src/main/java/com/pso/catappserver/CatAppServerApplication.java
@@ -0,0 +1,13 @@
+package com.pso.catappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatAppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatAppServerApplication.class, args);
+	}
+
+}
---
+++ project-cats/cat-app-server/src/test/java/com/pso/catappserver/CatAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pso.catappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatAppServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
