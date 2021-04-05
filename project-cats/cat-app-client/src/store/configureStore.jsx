bf8822d4f5ee7786cd60a68f2283fc697e7727b2
marsoon <asroq98@gmail.com>
2021-04-05 19:32:14 +0900

    [Update]signup logic

---
+++ project-cats/cat-app-client/src/store/configureStore.jsx
@@ -0,0 +1,35 @@
+import { createWrapper } from 'next-redux-wrapper'
+import { applyMiddleware, createStore, compose } from 'redux'
+// 크롬에서 debug하기 쉽게 extension 쓸거면
+import { composeWithDevTools } from 'redux-devtools-extension'
+import createSagaMiddleware from 'redux-saga'
+
+import reducer from '../reducers'
+import rootSaga from '../sagas'
+
+// 개발용
+// 함수 실행마다 로그해줘서 개발에 유용함
+// 이런 식으로 middleware를 직접 만들어 Redux기능을 확장시켜줄 수 있다
+const loggerMiddleware = ({ dispatch, getState }) => next => action => {
+	console.log(action)
+	return next(action)
+}
+
+const configureStore = () => {
+	const sagaMiddleware = createSagaMiddleware()
+	const middlewares = [sagaMiddleware, loggerMiddleware]
+	const enhancer =
+		process.env.NODE_ENV === 'production'
+			? compose(applyMiddleware(...middlewares)) //배포용
+			: composeWithDevTools(applyMiddleware(...middlewares))
+	const store = createStore(reducer, enhancer)
+	store.sagaTask = sagaMiddleware.run(rootSaga)
+
+	return store
+}
+
+const wrapper = createWrapper(configureStore, {
+	debug: process.env.NODE_ENV === 'development',
+})
+
+export default wrapper
