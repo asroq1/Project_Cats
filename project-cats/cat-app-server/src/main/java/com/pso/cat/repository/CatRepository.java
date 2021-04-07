fd7eced42d774c62fa0c5189f87bd9276b464531
hayeon17kim <hayeon17kim@gmail.com>
2021-04-07 23:57:02 +0900

    [Add] Cat Repository

--- project-cats/cat-app-server/src/main/java/com/pso/cat/repository/CatRepository.java
+++ project-cats/cat-app-server/src/main/java/com/pso/cat/repository/CatRepository.java
@@ -1,10 +1,18 @@
 package com.pso.cat.repository;
 
 import com.pso.cat.domain.Cat;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CatRepository extends JpaRepository<Cat, Long> {
+    @Modifying
+    @Query("UPDATE Cat c SET c.state = 0 where c.id = :id")
+    int inactive(@Param("no") Long id);
 
+    List<Cat> findAllByStateOrderByRegistrationDateDesc(@Param("state") int state);
 }
--- project-cats/cat-app-server/src/main/java/com/pso/cat/service/CatService.java
+++ project-cats/cat-app-server/src/main/java/com/pso/cat/service/CatService.java
@@ -1,6 +1,8 @@
 package com.pso.cat.service;
 
+import com.pso.cat.domain.Cat;
 import com.pso.cat.repository.CatRepository;
+import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,4 +12,17 @@ public class CatService {
     public CatService(CatRepository catRepository) {
         this.catRepository = catRepository;
     }
+
+    public Cat save(Cat cat) {
+        return catRepository.save(cat);
+    }
+
+    public Optional<Cat> read(Long id) {
+        return catRepository.findById(id);
+    }
+
+    public int delete(Long id) {
+        return catRepository.inactive(id);
+    }
+
 }
