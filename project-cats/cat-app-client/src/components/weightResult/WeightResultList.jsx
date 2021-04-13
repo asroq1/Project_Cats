04e933bb7a096f7bafa46f92aa0b9b887e0ad6c7
marsoon <asroq98@gmail.com>
2021-04-13 18:10:49 +0900

    [Update] list and graph compo

--- project-cats/cat-app-client/src/components/weightResult/WeightResultList.jsx
+++ project-cats/cat-app-client/src/components/weightResult/WeightResultList.jsx
@@ -1,7 +1,67 @@
-import React from 'react';
-
+import axios from 'axios';
+import React, { useEffect } from 'react';
+import styles from '../../styles/WeightResultList.module.css';
+const data = [
+    {
+        name: '2020-08-02',
+        wgt: 22.2,
+    },
+    {
+        name: '2020-08-03',
+        wgt: 22.4,
+    },
+    {
+        name: '2020-09-12',
+        wgt: 25.2,
+    },
+    {
+        name: '2020-09-22',
+        wgt: 25.0,
+    },
+    {
+        name: '2020-10-22',
+        wgt: 28.2,
+    },
+    {
+        name: '2020-12-22',
+        wgt: 30.2,
+    },
+    {
+        name: '2020-12-30',
+        wgt: 22.2,
+    },
+    {
+        name: '2020-01-30',
+        wgt: 25.2,
+    },
+    {
+        name: '2020-03-30',
+        wgt: 29.2,
+    },
+    {
+        name: '2020-04-20',
+        wgt: 29.2,
+    },
+    {
+        name: '2020-05-12',
+        wgt: 29.2,
+    },
+];
 const WeightResultTable = () => {
-    return <div>리슷흐</div>;
+    // 연동하면 여기에서 바로 조회하면 끝
+    // const test = axios.get('</백엔드 주소>');
+    return (
+        <div className={styles.list__container}>
+            {data.map((data) => {
+                return (
+                    <div className={styles.list__wrapper} key={data.name}>
+                        <p>{data.name}</p>
+                        <p>{data.wgt}kg</p>
+                    </div>
+                );
+            })}
+        </div>
+    );
 };
 
 export default WeightResultTable;
--- project-cats/cat-app-client/src/pages/weightResult.jsx
+++ project-cats/cat-app-client/src/pages/weightResult.jsx
@@ -2,6 +2,15 @@ import React from 'react';
 import { useState } from 'react';
 import WeightResultGraph from '../components/weightResult/WeightResultGraph';
 import WeightResultList from '../components/weightResult/WeightResultList';
+import styles from '../styles/weightResult.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faChartBar,
+    faListAlt,
+    faArrowRight,
+} from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+
 const WeightResult = () => {
     const [onList, setOnList] = useState(false);
     const chartHandler = () => {
@@ -12,10 +21,18 @@ const WeightResult = () => {
     };
     return (
         <>
-            <div>
-                <button onClick={chartHandler}>그래프</button>
-                <button onClick={listHandler}>리스트</button>
-            </div>
+            <header className={styles.header}>
+                {/* <button onClick={chartHandler}>그래프</button>
+                <button onClick={listHandler}>리스트</button> */}
+                <FontAwesomeIcon onClick={chartHandler} icon={faChartBar} />
+                <FontAwesomeIcon onClick={listHandler} icon={faListAlt} />
+                <Link to="/user/main">
+                    <FontAwesomeIcon
+                        icon={faArrowRight}
+                        className={styles.icon}
+                    />
+                </Link>
+            </header>
             {!onList && <WeightResultGraph />}
             {onList && <WeightResultList />}
         </>
