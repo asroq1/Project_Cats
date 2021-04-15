2bb2ccc0d025e770949c26cff8783f90f121566f
hayeon17kim <hayeon17kim@gmail.com>
2021-04-15 21:50:49 +0900

    [Add] dtos for auth and validation

--- project-cats/cat-app-server/src/main/java/com/pso/cat/dto/UserDto.java
+++ project-cats/cat-app-server/src/main/java/com/pso/cat/dto/UserDto.java
@@ -1,5 +1,7 @@
 package com.pso.cat.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -13,13 +15,18 @@ import lombok.Setter;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class LoginDto {
+public class UserDto {
 
     @NotNull
     @Size(min = 3, max = 50)
     private String email;
 
+    @JsonProperty(access = Access.WRITE_ONLY)
     @NotNull
     @Size(min = 3, max = 100)
     private String password;
+
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String nickname;
 }
