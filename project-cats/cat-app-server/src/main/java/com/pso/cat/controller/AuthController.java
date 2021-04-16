bb13680d4f366594b5a90650d0a0c566f5716ac6
hayeon17kim <hayeon17kim@gmail.com>
2021-04-16 10:41:39 +0900

    [Add] JWT

--- project-cats/cat-app-server/src/main/java/com/pso/cat/controller/AuthController.java
+++ project-cats/cat-app-server/src/main/java/com/pso/cat/controller/AuthController.java
@@ -1,10 +1,51 @@
 package com.pso.cat.controller;
 
-import com.pso.cat.domain.User;
-import java.util.Arrays;
-import java.util.List;
-import org.springframework.web.bind.annotation.GetMapping;
+import com.pso.cat.dto.LoginDto;
+import com.pso.cat.dto.TokenDto;
+import com.pso.cat.jwt.JwtFilter;
+import com.pso.cat.jwt.TokenProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api")
 public class AuthController {
 
+    private final TokenProvider tokenProvider;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+
+    public AuthController(TokenProvider tokenProvider,
+        AuthenticationManagerBuilder authenticationManagerBuilder) {
+        this.tokenProvider = tokenProvider;
+        this.authenticationManagerBuilder = authenticationManagerBuilder;
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<TokenDto> authorize(@Valid @RequestBody LoginDto loginDto) {
+
+        UsernamePasswordAuthenticationToken authenticationToken =
+            new UsernamePasswordAuthenticationToken(loginDto.getEmail(), loginDto.getPassword());
+
+        Authentication authentication = authenticationManagerBuilder.getObject()
+            .authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = tokenProvider.createToken(authentication);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
+
+        return new ResponseEntity<>(new TokenDto(jwt), httpHeaders, HttpStatus.OK);
+    }
 }
--- project-cats/cat-app-server/src/main/java/com/pso/cat/dto/UserDto.java
+++ project-cats/cat-app-server/src/main/java/com/pso/cat/dto/UserDto.java
@@ -1,32 +1,19 @@
 package com.pso.cat.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonProperty.Access;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
+import java.util.Date;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
 public class UserDto {
 
-    @NotNull
-    @Size(min = 3, max = 50)
+    private Long id;
+    private String name;
+
     private String email;
-
-    @JsonProperty(access = Access.WRITE_ONLY)
-    @NotNull
-    @Size(min = 3, max = 100)
-    private String password;
-
-    @NotNull
-    @Size(min = 3, max = 50)
+    private String pwd;
     private String nickname;
+    private String loginType;
+    private Date createdDate;
 }
