193aa2b16f1555d0488f020f910e3bdd04f059c6
marsoon <asroq98@gmail.com>
2021-04-27 17:51:04 +0900

    [Fix] conflict error

--- project-cats/catchoo-server/src/main/java/com/pso/cat/service/PostService.java
+++ project-cats/catchoo-server/src/main/java/com/pso/cat/service/PostService.java
@@ -2,6 +2,7 @@ package com.pso.cat.service;
 
 import com.pso.cat.dto.PostDto;
 import com.pso.cat.entity.Post;
+import com.pso.cat.entity.User;
 import com.pso.cat.repository.PostRepository;
 import java.util.List;
 import java.util.Optional;
@@ -17,43 +18,35 @@ public class PostService {
         this.postRepository = postRepository;
     }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 65ba0e40196949f576ca0196609d1ed2c1e9b1b6
     public Post save(Long userId, PostDto.Request postDto) {
         Post post = postDto.toEntity();
         post.setWriter(User.builder().id(userId).build());
         return postRepository.save(post);
-=======
-    public Post save(PostDto.Request post) {
-        return postRepository.save(post.toEntity());
->>>>>>> 5d4f34166559408907d3291a7bf2e12629d6b055
     }
 
-    public PostDto.SingleResponse read(Long id) {
-        Optional<Post> post = postRepository.findById(id);
-        post.ifPresent(p -> {
-            postRepository.updateViewCount(p.getId());
-        });
-        return PostDto.SingleResponse.ofEntity(post.get());
-    }
+        public PostDto.SingleResponse read (Long id){
+            Optional<Post> post = postRepository.findById(id);
+            post.ifPresent(p -> {
+                postRepository.updateViewCount(p.getId());
+            });
+            return PostDto.SingleResponse.ofEntity(post.get());
+        }
 
-    @Transactional
-    public Post modify(Long id, PostDto.Request postRequest) {
-        Post post = postRequest.toEntity();
-        post.setId(id);
-        return postRepository.save(post);
-    }
+        @Transactional
+        public Post modify (Long id, PostDto.Request postRequest){
+            Post post = postRequest.toEntity();
+            post.setId(id);
+            return postRepository.save(post);
+        }
 
-    @Transactional
-    public void remove(Long id) {
-        postRepository.inactive(id);
-    }
+        @Transactional
+        public void remove (Long id){
+            postRepository.inactive(id);
+        }
 
-    public List<PostDto.ListResponse> list() {
-        return postRepository
-            .findAllByStateOrderByCreatedDateDesc(1)
-            .stream().map(post -> PostDto.ListResponse.ofEntity(post)).collect(Collectors.toList());
+        public List<PostDto.ListResponse> list () {
+            return postRepository
+                    .findAllByStateOrderByCreatedDateDesc(1)
+                    .stream().map(post -> PostDto.ListResponse.ofEntity(post)).collect(Collectors.toList());
+        }
     }
-}
