f5ff56490ed023977f257cdb04f85e264647755c
haileykr <41278804+haileykr@users.noreply.github.com>
2021-05-04 17:58:05 +0900

    [Update] Updated Styling

--- cat-app-client/src/components/catsAdd/CatsAddContainer.jsx
+++ cat-app-client/src/components/catsAdd/CatsAddContainer.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 import CatImageUpload from './CatImageUpload';
 import CatsAdd from './CatsAdd';
-
-
 import styled from 'styled-components'
 
 const BackgroundColor = styled.div`
     background-color:${({theme})=>theme.beige};
     margin: 0;
     padding: 0;
+
+    min-height: 100vh; 
     `;
 
 const CatsAddContainer = ({}) => {
